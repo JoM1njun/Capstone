f7ca285e324afb26bc0a15abf3fecebd78b961c8
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-05-29 14:52:43 +0900

    Add files via upload

---
+++ js/db_connect.js
@@ -0,0 +1,17 @@
+// 서버 연결 확인을 위한 부분 (없어도 됨)
+window.onload = function () {
+  fetch("http://localhost:3000/api/db-connect")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        document.getElementById("db-status").innerHTML = "DB Connected";
+      } else {
+        document.getElementById("db-status").innerHTML =
+          "Connect Failed" + data.message;
+      }
+    })
+    .catch((error) => {
+      document.getElementById("db-status").innerHTML =
+        "서버에 접근 할 수 없음" + error;
+    });
+};
