bd214216979ab90ac4f73b4d47a18a00230d2285
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-06-04 09:40:23 +0900

    Update db_connect.js

--- js/db_connect.js
+++ js/db_connect.js
@@ -1,6 +1,6 @@
 // 서버 연결 확인을 위한 부분 (없어도 됨)
 window.onload = function () {
-  fetch("http://localhost:3000/api/db-connect")
+  fetch("http://43.201.78.22:3000/api/db-connect")
     .then((response) => response.json())
     .then((data) => {
       if (data.status === "success") {
