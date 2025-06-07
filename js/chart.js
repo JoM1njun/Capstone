b68ac2e9a92d8e52a45817ad60e2c4a82ab2ccc6
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-06-07 23:59:48 +0900

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -10,7 +10,7 @@ async function chart(itemId) {
 
     try {
         const response = await fetch(`https://capstone-back.fly.dev/api/management/movement/${itemId}`);
-        const semsors = await response.json();
+        const sensors = await response.json();
 
         const allTimestampsSet = new Set();
         sensors.forEach(sensor => {
